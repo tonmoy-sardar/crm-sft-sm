ca6da89917fad6ed08819a9becf9f579b4c30009
soumya bhattacharya <soumya.bhattacharya@shyamfuture.com>
2020-09-23 10:49:49 +0530

    footer modify

--- src/app/core/components/top-nav/top-nav.component.ts
+++ src/app/core/components/top-nav/top-nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
@@ -9,10 +9,15 @@ import { Router } from '@angular/router';
 export class TopNavComponent implements OnInit {
 
   navList: any = [];
+  selectedItem: any;
 
   constructor(
-    private router:Router
+    private router:Router,
+    private route:ActivatedRoute
   ) {
+    // this.router.events.subscribe((res) => { 
+    //   console.log(this.router.url,"Current URL");
+    // })
     
    }
 
@@ -52,8 +57,11 @@ export class TopNavComponent implements OnInit {
     this.selectNav()
   }
 
-  selectNav(){    
+  selectNav(){   
+    // console.log(this.route.snapshot.firstChild.url[0].path)
+    ////////////////////////////////// 
     var nav = this.router.url.substr(1)    
+    // console.log(nav)
     this.navList.forEach(element => {
       if(element.link == nav) element.selected = true;
       else element.selected = false;
@@ -63,6 +71,10 @@ export class TopNavComponent implements OnInit {
   goto(item){
     this.router.navigateByUrl('/'+item.link);
     setTimeout(() => {this.selectNav()}, 300); 
+  }
+
+  goToMenu(item) {
+    this.selectedItem = item;
   }
 
 }
